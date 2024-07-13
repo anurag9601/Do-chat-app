19914ecd611f8e214fa548cac6d895ab154c4391
hrisabhy <hrisabhyadav31@gmail.com>
2024-07-14 02:32:03 +0530

    Password hashing addded

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,4 +1,5 @@
-const User = require("../models/user.model")
+const User = require("../models/user.model");
+const bcrypt = require("bcryptjs");
 
 const signup = async (req, res) => {
     try {
@@ -14,25 +15,36 @@ const signup = async (req, res) => {
             return res.status(400).json({error: "Username already exists"});
         }
 
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+
         const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
 		const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
 
         const newUser = new User({
             fullName: fullname,
             username,
-            password,
+            password: hashedPassword,
             gender,
             profilePic: gender === 'male' ? boyProfilePic: girlProfilePic,
         })
 
-        await newUser.save();
+        if(newUser) {
+            
+            await newUser.save();
+    
+            res.status(201).json({
+                _id: newUser._id,
+                fullName: newUser.fullName,
+                username: newUser.username,
+                profilePic: newUser.profilePic,
+            })
 
-        res.status(201).json({
-            _id: newUser._id,
-            fullName: newUser.fullName,
-            username: newUser.username,
-            profilePic: newUser.profilePic,
-        })
+        } else {
+            res.status(400).json({error: "Invalid user data"});
+        }
+
     } catch (error) {
         console.log("Error in signup method", error.message);
         res.status(500).json({error: error.message})
