41bb714cc5c2245e6a7b1ff8bd4ed61f63d5f166
Hrisabh Yadav <hrisabhyadav31@gmail.com>
2024-07-13 22:27:51 +0530

    server.js setup

--- server/server.js
+++ server/server.js
@@ -1,1 +1,12 @@
-console.log("hey buddy let's start to write a code")
+const express = require('express');
+const app = express()
+
+const port = 4000;
+
+app.get("/", (req, res) => {
+    res.send("Hello world!")
+})
+
+app.listen(port, () => {
+    console.log(`Server running on ${port}`);
+})
